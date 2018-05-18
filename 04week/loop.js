bff72dc5c64c40e65d72ad8b94bcfb5750635905
Vanessa <tuohyvanessa@yahoo.com>
2018-05-17 20:48:25 -0500

    commiting

---
+++ 04week/loop.js
@@ -0,0 +1,29 @@
+'use strict'
+//Use a for loop to console.log each item in the array carsInReverse//
+let carsInReverse = {
+  'Honda',
+  'Audi',
+  'Porsch',
+  'Chevy',
+  'Mazda',
+  'BMW',
+  'Toyota',
+  'Ford',
+};
+for (let i = 0; < carsInReverse.length; i++) {
+  (console.log(carsInReverse[i])
+  };
+
+  //create an object (an array with keys and values) called persons with the following data//
+  //first name: Jane, last name: Doe, birthdate: jan 5, 1925//
+  //gender:female//
+  let persons = {
+    fName: "Jane",
+    lName: "Doe",
+    bDate: "Jan 5,1925",
+    gender: "female"
+  };
+  //Use a for... in loop and "if" state to console.log the value associated with the key birthdate//
+
+
+  //while loop, use a for loop to console log the numbers 1 to 1000
